49159961a6d906b262a3b9fd6535b4ec3e5f0465
PEzwartsEDU <pe.zwarts@student.alfa-college.nl>
2025-11-11 13:51:34 +0100

    Haha nu mijn eigendom!

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-def main():
-	print("Hello")
+from unittest import case
 
-if __name__ == '__main__':
-	main()
+print("Welk type procentsom wil je oplossen; maak je keuze:")
+print("[1] Hoeveel is x% van y?")
+print("[2] Hoeveel % is x van y")
+print("[3] x neemt toe met y%; hoeveel heb je nu?")
+choice = input("Welke optie kies je?")
+match choice:
+    case "1":
+        x = float(input("Wat is x?"))
+        y = float(input("Wat is y?"))
+        answer = x / 100 * y
+        print("De vraag is: hoeveel is " + str(x) + "% van " + str(y) + "?")
+        print("Het antwoord is: " + str(answer))
+        print("De berekening is: " + str(x) + "% = " + str(x/100) + " en " + str(x/100) + " x " + str(y) + " = " + str(answer) )
+    case "2":
+        x = float(input("Wat is x?"))
+        y = float(input("Wat is y?"))
+        answer = x / y * 100
+        print(f"De vraag is: hoeveel % is {x} van {y}?")
+        print(f"Het antwoord is: {answer}")
+        print(f"De berekening is: {x} : {y} = {x/y}; {x/y} x 100 = {answer:.0f}%")
